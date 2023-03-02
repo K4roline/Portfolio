8694fe88e6e5b4bdd47132b050d6c2ceb013f271
Karoline Correa <ckaroline226@gmail.com>
2023-03-01 22:25:32 -0300

    Desenvolvendo o projeto

---
+++ validate.js
@@ -0,0 +1,8 @@
+document.getElementById("formcontato__botao").addEventListener('click', MeuAlerta);
+
+function MeuAlerta() {
+
+    if (nome.value != "") {
+        alert('Obrigado sr(a) ' + nome.value + ', seus dados foram encaminhados com sucesso para contato!');
+    }
+}
