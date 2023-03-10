f49ba4a61eff4247a754eb4809f10c8a13374799
Karoline Correa <ckaroline226@gmail.com>
2023-03-09 23:51:34 -0300

    Desenvolvendo a responsividade do projeto

--- validate.js
+++ validate.js
@@ -1,8 +1,8 @@
-document.getElementById("formcontato__botao").addEventListener('click', MeuAlerta);
+document.getElementById("formcontato__botao").addEventListener('click', MeuAlerta)
 
 function MeuAlerta() {
 
     if (nome.value != "") {
-        alert('Obrigado sr(a) ' + nome.value + ', seus dados foram encaminhados com sucesso para contato!');
+        alert('Obrigado sr(a) ' + nome.value + ', seus dados foram encaminhados com sucesso para contato!')
     }
 }
